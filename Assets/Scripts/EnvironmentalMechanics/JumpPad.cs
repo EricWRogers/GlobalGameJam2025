868d9c8b0a39bb59815e0bedc357cc9996c6e09d
Aiden Thacker <aidenthacker86@gmail.com>
2025-01-25 11:08:54 -0600

    Working on the Jump Pad Animation

--- Assets/Scripts/EnvironmentalMechanics/JumpPad.cs
+++ Assets/Scripts/EnvironmentalMechanics/JumpPad.cs
@@ -4,12 +4,13 @@ public class JumpPad : MonoBehaviour
 {
     public Animator anim;
     public float springForce = 10f;
+    
     public void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.CompareTag("Player"))
         {
             anim.SetBool("didHit", true);
-            col.GetComponent<Rigidbody>();
+            col.GetComponent<Rigidbody>().AddForce(transform.forward * (col.GetComponent<Rigidbody>().linearVelocity.magnitude + springForce), ForceMode.Impulse);
         }
     }
 
