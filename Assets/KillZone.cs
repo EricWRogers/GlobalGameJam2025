a7f3ed0bc29ddfbfac8c7616bc8e1e8729322517
Ben <ben.lawrence0324@gmail.com>
2025-01-25 15:48:54 -0600

    tagged

--- Assets/KillZone.cs
+++ Assets/KillZone.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter(Collider other) {
+        if (other.GetComponent<BubbleController>()) {
+            
+        }
     }
 }
