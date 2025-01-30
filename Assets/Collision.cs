05d30aba41a121c8aff3430c9fa2150ab7c912ce
Logan <Loganhobson31@gmail.com>
2025-01-29 19:23:42 -0600

    Collision testing

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -1,7 +1,7 @@
-using Unity.VisualScripting;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Collision : NetworkBehaviour
 {
     public float knockbackMultiplier = 50f;
     private Rigidbody rb;
@@ -12,34 +12,49 @@ public class Collision : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("MapMechanic")) {
-            Debug.Log("Entering");
-            GetComponent<BubbleController>().lastVelocity = Vector3.zero;  
-        }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsServer)
+        {
 
 
-        if (other.CompareTag("Player")) {
-            Debug.Log("Player Found");
-            var otherRb = other.GetComponentInParent<Rigidbody>();
+            if (other.CompareTag("MapMechanic"))
+            {
+                Debug.Log("Entering");
+                GetComponent<BubbleController>().lastVelocity = Vector3.zero;
+            }
 
-            if (otherRb != null) {
-                float myVelocity = rb.linearVelocity.magnitude;
-                float otherVelocity = otherRb.linearVelocity.magnitude;
-                Debug.Log("knock");
-                if (myVelocity > otherVelocity) {
-                    Vector3 dir = (transform.position - other.transform.position).normalized;
-                    otherRb.linearVelocity = dir * myVelocity * knockbackMultiplier;
-                } else if (myVelocity < otherVelocity) {
-                    Vector3 knockbackDirection = (other.transform.position - transform.position).normalized;
-                    rb.linearVelocity = knockbackDirection * otherVelocity * knockbackMultiplier;
+
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("Player Found");
+                var otherRb = other.GetComponentInParent<Rigidbody>();
+
+                if (otherRb != null)
+                {
+                    float myVelocity = rb.linearVelocity.magnitude;
+                    float otherVelocity = otherRb.linearVelocity.magnitude;
+                    Debug.Log("knock");
+                    if (myVelocity > otherVelocity)
+                    {
+                        Vector3 dir = (transform.position - other.transform.position).normalized;
+                        otherRb.linearVelocity = dir * myVelocity * knockbackMultiplier;
+                    }
+                    else
+                     if (myVelocity < otherVelocity)
+                    {
+                        Vector3 knockbackDirection = (other.transform.position - transform.position).normalized;
+                        rb.linearVelocity = knockbackDirection * otherVelocity * knockbackMultiplier;
+                    }
                 }
             }
         }
     }
 
-    private void OnTriggerStay(Collider other) {
-        if (other.CompareTag("MapMechanic")) {
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MapMechanic"))
+        {
             Debug.Log("Staying");
             GetComponent<BubbleController>().lastVelocity = Vector3.zero;
         }
